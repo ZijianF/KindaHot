b3dd912fe8f9ca83921652d7fa8ebf431b71a56c
Yuxuan Huang <yxuhuang@ucdavis.edu>
2021-06-10 04:12:48 +0800

    Saved scene

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -24,10 +24,14 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] private bool ShowBot = false;
 
+    [SerializeField] GameObject Gun1;
+
     private bool InLineOfSight = false;
 
     private Animator animator;
-    
+
+    private EnemyGunController GunController;
+
 
     // Should leave space for weapon prefab;
 
@@ -35,16 +39,19 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
-        Vector3 spawnLocation = new Vector3(this.gameObject.transform.position.x + SpawnRadius * Random.Range(0f, SpawnRadius) * (Random.Range(0, 2) *2 -1), 0f , this.gameObject.transform.position.z + SpawnRadius * Random.Range(0f, SpawnRadius) * (Random.Range(0, 2) *2 -1));
+        Vector3 spawnLocation = new Vector3(this.gameObject.transform.position.x + SpawnRadius * Random.Range(0f, SpawnRadius) * (Random.Range(0, 2) * 2 - 1), 0f, this.gameObject.transform.position.z + SpawnRadius * Random.Range(0f, SpawnRadius) * (Random.Range(0, 2) * 2 - 1));
         this.transform.position = spawnLocation;
         // Head = GameObject.FindGameObjectWithTag("head");
         // Target = GameObject.Find("Player/The Adventurer Blake/armature/Bone/hips/Spine/Spine1/chest/neck/head");
+
+        GunController = Gun1.GetComponent<EnemyGunController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.Dead)
+        this.InLineOfSight = false;// reset line of sight
+        if (this.Dead)
         {
             animator.SetBool("Dead", false);
             animator.SetBool("Exit", true);
@@ -59,7 +66,7 @@ public class EnemyController : MonoBehaviour
         Debug.DrawRay(Head.transform.position, rayDirection * 10.0f, Color.blue);
         // Debug.Log("Player postion " + Target.transform.position.x+ " " + Target.transform.position.y + " " + Target.transform.position.z);
         // Debug.Log("Head postion " + Head.transform.position.x+ " " + Head.transform.position.y + " " + Head.transform.position.z);
-        if (Physics.Raycast(Head.transform.position, rayDirection, out hit)) 
+        if (Physics.Raycast(Head.transform.position, rayDirection, out hit))
         {
             // Debug.Log("postion " + hit.point.x+ " " + hit.point.y + " " + hit.point.z);
             bool hitPlayer = false;
@@ -75,31 +82,34 @@ public class EnemyController : MonoBehaviour
                 // Saw the target
                 Debug.DrawRay(Head.gameObject.transform.position, rayDirection * 10000f, Color.yellow);
                 // Debug.Log("Name " + hit.collider.gameObject.name);
-                
-            } else
+
+            }
+            else
             {
                 this.InLineOfSight = false;
                 // Did not find target
                 Debug.DrawRay(Head.gameObject.transform.position, rayDirection * 10000f, Color.red);
             }
         }
-        if(this.InLineOfSight) 
+        if (this.InLineOfSight)
         {
             Vector3 direction = rayDirection.normalized;
             Quaternion lookRotation = Quaternion.LookRotation(rayDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * RotationSpeed);
+            GunController.RenewDirection(hit.point);//- Camera.main.transform.position);
+
         }
         // TakeDamage();
-        if(this.ShowBot)
+        if (this.ShowBot)
         {
             return;
         }
     }
-    
+
     public void TakeDamage()
     {
         this.Health -= 1;
-        if(this.Health <= 0)
+        if (this.Health <= 0)
         {
             this.Dead = true;
             EnterDeathAnimation();
@@ -107,11 +117,12 @@ public class EnemyController : MonoBehaviour
     }
     private void EnterDeathAnimation()
     {
-        if(ShowBot)
+        if (ShowBot)
         {
             animator.SetBool("Dead", true);
         }
-        else{
+        else
+        {
             // animator.SetBool("Idle", false);
             // animator.SetBool("IsMovingForward", false);
             // animator.SetBool("IsMovingBackward", false);
@@ -126,5 +137,6 @@ public class EnemyController : MonoBehaviour
         animator.SetBool("IsMovingLeft", false);
         animator.SetBool("Dead", true);
     }
-        
+
+
 }
